4f85e5d392dd427403cf19102a90075bb638eb4e
Aditya Porwal <aadityporwal96@gmail.com>
2025-08-30 19:22:05 +0530

    Add files via upload

---
+++ Add_String.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include<algorithm>
+using namespace std;
+
+string add(string num1, string num2){
+    int carry=0, sum;
+    string ans;
+    int index1 = num1.size()-1;
+    int index2 = num2.size()-1;
+
+    while (index2 >= 0)
+    {
+        sum = (num1[index1] - '0') + (num2[index2] - '0') + carry;
+        carry = sum / 10;
+        char c = sum%10 + '0';
+        ans += c;
+        index1--;
+        index2--;
+    }
+
+    while(index1>=0) {
+        sum = (num1[index1] - '0') + carry;
+        carry = sum / 10;
+        char c = sum%10 + '0';
+        ans += c;
+        index1--;
+    }
+
+    if(carry)
+    ans+='1';
+
+    reverse(ans.begin(),ans.end());
+    return ans;
+
+}
+
+int main() {
+    string num1,num2;
+    cin >> num1 >> num2;
+
+    if(num1.size()>=num2.size())
+    cout << add(num1,num2);
+
+    else
+    cout << add(num2,num1);
+    
+}
